c541770f50450bd95211c60d8e7c81627bc37046
phher802 <pher024@gmail.com>
2021-08-05 21:39:10 -0500

    Add Customer model

---
+++ EmployeeDirectory/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeDirectory.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string CustomerName {get; set;}
+
+        [Phone]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
+        public string PhoneNumber { get; set; }
+
+    }
+}
--- EmployeeDirectory/Models/Project.cs
+++ EmployeeDirectory/Models/Project.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,15 @@ namespace EmployeeDirectory.Models
 {
     public class Project
     {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string CustomerContact { get; set; }
+
+        public string Deadline { get; set; }
     }
 }
