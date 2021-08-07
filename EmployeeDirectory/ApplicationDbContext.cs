1b1dddfca47eb3379ba89982f367cc09a7da54e2
phher802 <pher024@gmail.com>
2021-08-06 22:47:26 -0500

    Add Applit sqlserver

---
+++ EmployeeDirectory/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeDirectory.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeDirectory
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+
+    }
+}
--- EmployeeDirectory/Startup.cs
+++ EmployeeDirectory/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 
 namespace EmployeeDirectory
 {
@@ -23,7 +25,10 @@ namespace EmployeeDirectory
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllersWithViews();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
